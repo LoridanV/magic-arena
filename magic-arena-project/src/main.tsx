3e893bb004700d4a93e97aa7731addab6d7406b3
Loridan Varta - POO -R6 <vartalorik@gmail.com>
2026-06-17 00:15:19 +0300

    fix tanstack start conflict

--- magic-arena-project/src/main.tsx
+++ magic-arena-project/src/main.tsx
@@ -1,16 +1,19 @@
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import "./styles.css";
 
 const router = createRouter({ routeTree });
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
